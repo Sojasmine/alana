d41aab209766bac0c501bc098dae654a6e048673
Sojasmine <sojasmine.gjerstad@gmail.com>
2021-09-01 22:29:25 +0000

    Added stripe public key and client secret key

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JKq6NFkzat2NiNoXrJtCj8X0TaHSdg9ebfxVgDM5stvIGAYA1zUFTNi9Y40f3m50ZyjCrc6ja7GWl5UCe1UPizb00V8OwpQY6',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
