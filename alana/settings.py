608bc0ab2ce4561a41df716b36d94ee40aada573
Sojasmine <sojasmine.gjerstad@gmail.com>
2021-09-04 22:21:40 +0000

    Removed secret key and set debug to development

--- alana/settings.py
+++ alana/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w2v$kl3uh3k2rzbb3c4e%g47n$wf30+6yboj6m7xd^eki1b6u&'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['alana-app.herokuapp.com', 'localhost']
 
