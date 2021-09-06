c1bdaab5bc1005d0cf6e0806f576b2a8d344c7d4
Sojasmine <sojasmine.gjerstad@gmail.com>
2021-09-06 09:32:03 +0000

    Added contact url

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+from django.db import models
+
+
+class Contact(models.Model):
+    ''' contact model and fields'''
+    subject = models.CharField(max_length=120, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    message = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return self.subject
