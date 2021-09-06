7b29f253d9b676ff3c84f0547c6f0c27b53348c7
Sojasmine <sojasmine.gjerstad@gmail.com>
2021-09-06 20:40:25 +0000

    Modified contact url and views

--- home/views.py
+++ home/views.py
@@ -13,34 +13,5 @@ def index(request):
 
 
 def contact(request):
-    '''This view returns contact page and
-       posts the contact form information to the db
-    '''
-    if request.method == "POST":
-        form = ContactForm(request.POST)
-        if form.is_valid:
-            form.subject = request.POST['subject'],
-            form.message = request.POST['message'],
-            form.email = request.POST['email'],
-            form.save()
-            user_email = ''.join(form.email)
-            messages.success(request,
-                             f"Thanks { user_email }, Your message has been sent successfully.\
-                             We will be in touch shortly.")
-            return redirect("home")
-        else:
-            messages.error(request,
-                           'Error: Your message could not be sent \
-                            please try again later.')
-            return redirect('home')
-    else:
-        form = ContactForm()
-
-    template = 'home/contact_us.html'
-    context = {
-        'form': form,
-    }
-
-    return render(request,
-                  template,
-                  context)
+   
+   return render(request, 'home/contact_us.html')
