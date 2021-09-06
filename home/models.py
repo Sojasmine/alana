c71c3e8a0c4ffa86f3db68d41d2423e026b30fc6
Sojasmine <sojasmine.gjerstad@gmail.com>
2021-09-06 10:31:31 +0000

    Coorection of a double import of the models

--- home/models.py
+++ home/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-
-from django.db import models
-
-
 class Contact(models.Model):
     ''' contact model and fields'''
     subject = models.CharField(max_length=120, null=False, blank=False)
