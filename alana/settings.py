185e4828fd5fefbc7ec993445df5c66137c82328
Sojasmine <sojasmine.gjerstad@gmail.com>
2021-11-30 09:48:14 +0000

    Removed the postgress url database and uncomment database_url.

--- alana/settings.py
+++ alana/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'f1y438mefvon$r@@o1r9e86t_cay*k_h=(@2l_&6mw#(=uop!2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if 'DEVELOPMENT' in os.environ:
@@ -137,6 +137,8 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
