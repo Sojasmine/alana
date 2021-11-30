13a810cc809ce37d4e01ff9d322244fb47dacfaa
Sojasmine <sojasmine.gjerstad@gmail.com>
2021-11-30 02:37:21 +0000

    Removed the SECRET KEY agian for safety

--- alana/settings.py
+++ alana/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'f1y438mefvon$r@@o1r9e86t_cay*k_h=(@2l_&6mw#(=uop!2')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if 'DEVELOPMENT' in os.environ:
